8007e13435b1970f92a2fda90e45fd9223da4493
kush-raj-nit061 <kushpvt1@gmail.com>
2023-08-26 11:16:44 +0530

    Messaging user holder completed

--- app/src/main/java/com/example/friendship/MailFragment.java
+++ app/src/main/java/com/example/friendship/MailFragment.java
@@ -1,6 +1,7 @@
 package com.example.friendship;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.transition.TransitionManager;
 import android.view.LayoutInflater;
@@ -8,11 +9,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.friendship.Model.Chatlist;
+
+import com.example.friendship.Model.User;
 import com.github.sshadkany.shapes.CircleView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MailFragment extends Fragment {
     public MailFragment(){}
@@ -22,12 +46,36 @@ public class MailFragment extends Fragment {
     private ConstraintLayout constraintLayout;
     private CircleView imageViewPhoto;
 
+    private RecyclerView recyclerView;
+
+    Typeface MR, MRR;
+    private UsersAdapter userAdapter;
+    private List<User> mUsers;
+    FrameLayout frameLayout;
+//    TextView es_descp, es_title;
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+
+    private List<Chatlist> usersList;
+    static OnItemClick onItemClick;
+
+
+    public static MailFragment newInstance(OnItemClick click) {
+
+        onItemClick = click;
+        Bundle args = new Bundle();
+
+        MailFragment fragment = new MailFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
     @SuppressLint("MissingInflatedId")
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_testing, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_mail_fragment, container, false);
 
-        // changing the status bar color to transparent
         Window w = requireActivity().getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
@@ -53,6 +101,94 @@ public class MailFragment extends Fragment {
             }
         });
 
+        MRR = Typeface.createFromAsset(getContext().getAssets(), "fonts/myriadregular.ttf");
+        MR = Typeface.createFromAsset(getContext().getAssets(), "fonts/myriad.ttf");
+
+        recyclerView = view.findViewById(R.id.recConnection);
+//        frameLayout = view.findViewById(R.id.es_layout);
+//        es_descp = view.findViewById(R.id.es_descp);
+//        es_title = view.findViewById(R.id.es_title);
+
+//        es_descp.setTypeface(MR);
+//        es_title.setTypeface(MRR);
+
+
+
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        usersList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chatlist").child(fuser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chatlist chatlist = snapshot.getValue(Chatlist.class);
+                    usersList.add(chatlist);
+                }
+                if(usersList.size()==0){
+//                    frameLayout.setVisibility(View.VISIBLE);
+                }
+                else{
+//                    frameLayout.setVisibility(View.GONE);
+                }
+
+                chatList();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        updateToken(String.valueOf(FirebaseMessaging.getInstance().getToken()));
+
+
         return view;
     }
+
+    private void updateToken(String token){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
+        Token token1 = new Token(token);
+        reference.child(fuser.getUid()).setValue(token1);
+    }
+
+    private void chatList() {
+        mUsers = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    for (Chatlist chatlist : usersList){
+                        if (user!= null && user.getId()!=null && chatlist!=null && chatlist.getId()!= null &&
+                                user.getId().equals(chatlist.getId())){
+                            mUsers.add(user);
+                        }
+                    }
+                }
+
+
+                userAdapter = new UsersAdapter(getContext(), onItemClick,mUsers, true);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/friendship/OnItemClick.java
@@ -0,0 +1,7 @@
+package com.example.friendship;
+
+import android.view.View;
+
+public interface OnItemClick {
+    public void onItemCLick(String uid, View view);
+}
