38c74bed0bfcf500d14eecb907fa43b3b2b4eeae
kush-raj-nit061 <kushpvt1@gmail.com>
2023-08-29 01:25:17 +0530

    Profile Activity connected to main but profile activity remains to update data

--- app/src/main/java/com/example/friendship/MainActivity.java
+++ app/src/main/java/com/example/friendship/MainActivity.java
@@ -20,13 +20,18 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
 import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
 import com.wwdablu.soumya.lottiebottomnav.FontBuilder;
@@ -45,9 +50,12 @@ public class MainActivity extends AppCompatActivity implements ILottieBottomNavC
     boolean doubleBackToExitPressedOnce = false;
     ArrayList<MenuItem> list;
     DatabaseReference reference;
+    DatabaseReference reference2;
     FirebaseAuth fAuth = FirebaseAuth.getInstance();
     FlowingDrawer mDrawer;
     ImageView iv_menu;
+    ImageView ivDrawer;
+    CardView cvProfile;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -57,6 +65,34 @@ public class MainActivity extends AppCompatActivity implements ILottieBottomNavC
 
         bottomNav   = findViewById(R.id.bottom_nav);
         iv_menu = findViewById(R.id.iv_Menu);
+        cvProfile = findViewById(R.id.cvProfile);
+        ivDrawer = findViewById(R.id.ivDrawer);
+        reference2 = FirebaseDatabase.getInstance().getReference("students");
+
+        reference2.child(fAuth.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Glide.with(getApplicationContext()).load(snapshot.child("purl").getValue()).into(ivDrawer);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+        cvProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
         iv_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
