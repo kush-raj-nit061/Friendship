ccc69258b96e4a057185dbdda4eb807b92f125e5
kush-raj-nit061 <kushpvt1@gmail.com>
2023-08-30 00:13:36 +0530

    Featured People added

--- app/src/main/java/com/example/friendship/GiftsFragment.java
+++ app/src/main/java/com/example/friendship/GiftsFragment.java
@@ -4,12 +4,22 @@ package com.example.friendship;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class GiftsFragment extends Fragment {
+
+    RecyclerView recFeatured;
+    FeaturedAdapter userAdapter;
+    DatabaseReference reference;
 
     public GiftsFragment() {
         // Required empty public constructor
@@ -19,6 +29,24 @@ public class GiftsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_gifts_fragment, container, false);
+        View view = inflater.inflate(R.layout.activity_gifts_fragment, container, false);
+
+
+        recFeatured  = view.findViewById(R.id.recFeatured);
+        recFeatured.setLayoutManager(new LinearLayoutManager(getContext()));
+        recFeatured.setItemAnimator(null);
+
+
+        FirebaseRecyclerOptions<FeaturedModel> options =
+                new FirebaseRecyclerOptions.Builder<FeaturedModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Featured"), FeaturedModel.class)
+                        .build();
+        userAdapter=new FeaturedAdapter(options);
+        recFeatured.setAdapter(userAdapter);
+        userAdapter.startListening();
+
+
+
+        return view;
     }
 }
--- app/src/main/java/com/example/friendship/Testing2.java
+++ app/src/main/java/com/example/friendship/Testing2.java
@@ -13,35 +13,10 @@ import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
 public class Testing2 extends AppCompatActivity {
 
 
-    FlowingDrawer mDrawer;
-    ImageView iv_menu;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_testing2);
-        iv_menu = findViewById(R.id.iv_Menu);
-        iv_menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mDrawer.openMenu(true);
-            }
-        });
+        setContentView(R.layout.activity_testing);
 
-        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
-        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
-        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
-            @Override
-            public void onDrawerStateChange(int oldState, int newState) {
-                if (newState == ElasticDrawer.STATE_CLOSED) {
-                    Log.i("MainActivity", "Drawer STATE_CLOSED");
-                }
-            }
-
-            @Override
-            public void onDrawerSlide(float openRatio, int offsetPixels) {
-                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
-            }
-        });
     }
 }
