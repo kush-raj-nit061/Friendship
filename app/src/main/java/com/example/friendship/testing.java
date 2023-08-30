05df95f12dda9596a839e163168ab8412df300c0
kush-raj-nit061 <kushpvt1@gmail.com>
2023-08-30 16:15:35 +0530

    Developers card prepared

--- app/src/main/java/com/example/friendship/testing.java
+++ app/src/main/java/com/example/friendship/testing.java
@@ -9,6 +9,6 @@ public class testing extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_testing);
+        setContentView(R.layout.developer_item);
     }
 }
