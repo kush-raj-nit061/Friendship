01b43730fa684ff6280e046847db1e81956a1c30
kush-raj-nit061 <kushpvt1@gmail.com>
2023-09-13 13:06:26 +0530

    final commit 0.4

--- app/src/main/java/com/example/friendship/SeeCollaborationDetail.java
+++ app/src/main/java/com/example/friendship/SeeCollaborationDetail.java
@@ -72,7 +72,6 @@ public class SeeCollaborationDetail extends AppCompatActivity {
             }
         });
 
-
         btnCollab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
